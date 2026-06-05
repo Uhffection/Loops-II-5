f3f6d6d2da1cb4a5bb70229a62fa5e256fe0d8e6
Uhffection <142746146+Uhffection@users.noreply.github.com>
2026-06-05 18:39:40 +0000

    Create Functions findProduct and findProduct2

--- main.js
+++ main.js
@@ -1,1 +1,56 @@
 // Write your code below
+
+/* All 'return' statements are included just
+ for me to practice storing data after running
+ the function */
+
+ function findProduct(num1,num2) {
+    let product = 1
+    
+    if (num1 > num2) {
+        
+        console.log(0);
+        
+        return 0;
+    }
+
+    for (i = num1;i < num2; i++) {
+        product *= i
+    }
+
+    console.log(product);
+    
+    return product;
+}
+
+findProduct(1,4);
+
+// While Loop Configuration
+
+function findProduct2(num1,num2) {
+     
+    let product = 1;
+
+    let i = num1;
+
+    if (num1 > num2) {
+
+        console.log(0)
+
+        return 0
+    }
+    
+    while (i < num2) {
+
+        product *= i;
+
+        i++;
+
+    }
+
+    console.log(product);
+
+    return product;
+}
+
+findProduct(1,4);
